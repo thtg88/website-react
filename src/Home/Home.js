a0084ac204baf5fde59c8db05c9b0ff5a6a4dd20
Marco Marassi <info@marco-marassi.com>
2018-07-12 21:31:05 +0100

    Added robots.txt. Added Google Analytics tracking code and package. Removed Hotjar package and tracking. Package updates.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,53 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
-import { hotjar } from 'react-hotjar';
+import ReactGA from 'react-ga';
 import About from './About/About.js';
 import Contact from './Contact/Contact.js';
 import './Home.css';
 import icon from './icon.png';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.handleLoad = this.handleLoad.bind(this);
-    this.state = {
-      opacity: typeof window !== 'undefined' ? 0 : 1,
-      display: typeof window !== 'undefined' ? 'none' : 'block',
+    constructor(props) {
+        super(props);
+        this.handleLoad = this.handleLoad.bind(this);
+        this.state = {
+            opacity: typeof window !== 'undefined' ? 0 : 1,
+            display: typeof window !== 'undefined' ? 'none' : 'block',
+        }
+
+        ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
     }
 
-    hotjar.initialize(process.env.REACT_APP_HOTJAR_SITE_ID, process.env.REACT_APP_HOTJAR_VERSION);
-  }
+    componentDidMount() {
+        ReactGA.pageview('/');
+    }
 
-  render() {
-    return (
-      <div style={{height:'100%'}}>
-        <div className="loading-container" style={{zIndex: 2000, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: '#337ab7', display:this.state.display}}>
-          <div className="loader" style={{width: 6+'px', height: 6+'px', animation: 'typing 1s linear infinite alternate', margin: '50vh auto', position: 'relative', left: '-12px', backgroundColor: '#ffffff'}}></div>
-        </div>
-        <header id="top" className="header" onLoad={this.handleLoad} style={{opacity:this.state.opacity}} title="Picture taken at: Cala Blanca, Ciutadella de Menorca, Spain.">
-          <div className="text-vertical-center">
-            <h1>Hi, I'm Marco Marassi.</h1>
-            <h3>Web Developer by day, Sleeper by night.</h3>
-            <p>What did you think I was? A superhero or something?</p>
-            <div id="profile-icon-container">
-              <img id="profile-icon" src={icon} alt="Icon Logo" title="This looks like my face." className="img-responsive img-circle" />
+    handleLoad() {
+        if(this.state.opacity === 0) {
+            this.setState({
+                opacity: 1,
+                display: 'none'
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div style={{height:'100%'}}>
+                <div className="loading-container" style={{zIndex: 2000, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: '#337ab7', display:this.state.display}}>
+                    <div className="loader" style={{width: 6+'px', height: 6+'px', animation: 'typing 1s linear infinite alternate', margin: '50vh auto', position: 'relative', left: '-12px', backgroundColor: '#ffffff'}}></div>
+                </div>
+                <header id="top" className="header" onLoad={this.handleLoad} style={{opacity:this.state.opacity}} title="Picture taken at: Cala Blanca, Ciutadella de Menorca, Spain.">
+                    <div className="text-vertical-center">
+                        <h1>Hi, I'm Marco Marassi.</h1>
+                        <h3>Web Developer by day, Sleeper by night.</h3>
+                        <p>What did you think I was? A superhero or something?</p>
+                        <div id="profile-icon-container">
+                            <img id="profile-icon" src={icon} alt="Icon Logo" title="This looks like my face." className="img-responsive img-circle" />
+                        </div>
+                        <Link to="about" smooth={true} duration={1000} className="btn btn-dark btn-lg">But wait, there's more!</Link>
+                    </div>
+                </header>
+                <About name="about" />
+                <Contact />
             </div>
-            <Link to="about" smooth={true} duration={1000} className="btn btn-dark btn-lg">But wait, there's more!</Link>
-          </div>
-        </header>
-        <About name="about" />
-        <Contact />
-      </div>
-    );
-  }
-
-  handleLoad() {
-    if(this.state.opacity === 0) {
-      this.setState({
-        opacity: 1,
-        display: 'none'
-      });
+        );
     }
-  }
 }
 export default Home;
