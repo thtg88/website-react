2ef2cd243e53cb575f6432634d9cec1d2925b4ce
Marco Marassi <thtg88@users.noreply.github.com>
2018-12-30 22:15:45 +0000

    Added sentry dependency via npm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { init } from '@sentry/browser';
 import ErrorBoundary from './ErrorBoundary';
 import Home from './Home';
 import Page404 from './Page404';
 
+const {
+    NODE_ENV,
+    REACT_APP_SENTRY_KEY,
+    REACT_APP_SENTRY_PROJECT_ID
+} = process.env;
+
 class App extends Component {
-
     componentDidMount() {
-        if (window.Raven && process.env.NODE_ENV === 'production') {
-            const { REACT_APP_SENTRY_KEY, REACT_APP_SENTRY_PROJECT_ID } = process.env;
-
-            const raven_url = 'https://'+REACT_APP_SENTRY_KEY+'@sentry.io/'+REACT_APP_SENTRY_PROJECT_ID;
-
-            window.Raven.config(raven_url).install();
+        if(
+            NODE_ENV === 'production'
+            && typeof REACT_APP_SENTRY_KEY !== 'undefined'
+            && REACT_APP_SENTRY_KEY !== null
+            && REACT_APP_SENTRY_KEY !== ''
+            && typeof REACT_APP_SENTRY_PROJECT_ID !== 'undefined'
+            && REACT_APP_SENTRY_PROJECT_ID !== null
+            && REACT_APP_SENTRY_PROJECT_ID !== ''
+        ) {
+            // Initialize Sentry error tracking
+            init({
+                dsn: `https://${REACT_APP_SENTRY_KEY}@sentry.io/${REACT_APP_SENTRY_PROJECT_ID}`
+            });
         }
     }
 
